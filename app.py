fa2ae2416bb12a267af9693dc64cb6ec49904fa7
aromol <102617639+aromol@users.noreply.github.com>
2022-06-22 02:35:03 -0400

    Update app.py
    insert good

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ if streamlit.button('Get Fruit Load List'):
 #funcion de insersion a Snowflake
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list values ('from stramlit')")
+    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
     return "Tranks fro adding "+ new_fruit
   
 if streamlit.button('Add a Fruit to the List'):
