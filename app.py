b96c9da1be5d64b3293e04feca91942fa9857e25
aromol <102617639+aromol@users.noreply.github.com>
2022-06-22 00:47:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
-import streamlit
+import streamlit 
+import pandas
 
-streamlit.title("titulo")
+#Añadimos el titulo de nuestro sitio web
+
+streamlit.title("Listado de comida")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
