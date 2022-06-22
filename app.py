09ae7175fd90f6fc1c1d72549e5a13a31ddb0a07
aromol <102617639+aromol@users.noreply.github.com>
2022-06-22 02:34:08 -0400

    Update app.py
    insert

--- app.py
+++ app.py
@@ -62,7 +62,16 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-
+#funcion de insersion a Snowflake
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from stramlit')")
+    return "Tranks fro adding "+ new_fruit
+  
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
 
 
 
